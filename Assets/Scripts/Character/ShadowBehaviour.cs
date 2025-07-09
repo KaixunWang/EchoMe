f2c11d6acd92d20393ea3b6fdd0366fba8ed2ffc
haooo-yue <2313583830@qq.com>
2025-07-09 19:43:50 +0800

    MovingPlatform

--- Assets/Scripts/Character/ShadowBehaviour.cs
+++ Assets/Scripts/Character/ShadowBehaviour.cs
@@ -413,6 +413,12 @@ public class ShadowBehaviour : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.gameObject.CompareTag("MovingPlatform"))
+        {
+            transform.SetParent(other.transform); // 设置玩家为移动平台的子物体
+            rb.gravityScale = 0f; // 禁用重力
+            Debug.Log("Player entered MovingPlatform");
+        }
         if (other.gameObject.name == "Switch")
         {
             Debug.Log("Player is near Switch");
@@ -433,6 +439,12 @@ public class ShadowBehaviour : MonoBehaviour
 
     void OnTriggerExit2D(Collider2D other)
     {
+        if (other.gameObject.CompareTag("MovingPlatform"))
+        {
+            transform.SetParent(null); // 设置玩家为移动平台的子物体
+            rb.gravityScale = 3.5f; // 恢复重力
+            Debug.Log("Player exited MovingPlatform");
+        }
         if (other.gameObject.name == "Switch")
         {
             Debug.Log("Player is out Switch");
